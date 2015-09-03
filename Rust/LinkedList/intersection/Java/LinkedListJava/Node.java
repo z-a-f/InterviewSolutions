87380defd5807e7f46a584615658d9c7c4e96f4c
Zafar Takhirov <cc.rafaz@zafar.cc>
2015-09-02 22:29:02 -0400

    Added intersection README

---
+++ Rust/LinkedList/intersection/Java/LinkedListJava/Node.java
@@ -0,0 +1,39 @@
+package LinkedListJava;
+
+import java.lang.Comparable;
+
+public class Node<T extends Comparable<T>>{
+	public Node(T e, Node<T> n, Node<T> a) {
+		this._next = n;
+		this._elem = e;
+		this._arb = a;
+	}
+	public Node(T e, Node<T> n) {
+		this._next = n;
+		this._elem = e;
+		this._arb = null;
+	}
+	public Node(T e) {
+		this._next = null;
+		this._elem = e;
+		this._arb = null;
+	}
+	public Node() {
+		this._next = null;
+		this._arb = null;
+		// this._elem = 0;
+	}
+
+	public T value() { return this._elem; }
+	public Node<T> next() { return this._next; }
+	public Node<T> arb() { return this._arb; }
+	public void setValue(T e) { this._elem = e; }
+	public void setNext(Node<T> n) { this._next = n; }
+	public void setArb(Node<T> n) { this._arb = n; }
+	
+	protected T _elem;
+	protected Node<T> _next;
+	protected Node<T> _arb;
+
+	// Linked List is friend as long as it is in the same package
+}
---
+++ Rust/LinkedList/intersection/Java/solution1.java
@@ -0,0 +1,49 @@
+import LinkedListJava.*;
+// import java.lang.Comparable;
+
+/* This solution doesn't work so far :'( */
+
+class solution1 {
+    // This is the solution :)
+    public static <T extends Comparable<T>> Node<T> sorted_insert(Node<T> head, Node<T> node) {
+        if (node == null) return head;
+
+        if (head == null || node.value() <= head.value()) {
+            node.setNext(head);
+            return node;
+        }
+
+        Node<T> curr = head;
+
+        while (curr.next() != null && (curr.next().value() < node.value()))
+            curr = curr.next();
+
+        node.setNext(curr.next());
+        curr.setNext(node);
+
+        return head;
+    }
+
+    public static <T extends Comparable<T>> Node<T> insertion_sort(Node<T> head) {
+        Node<T> sorted = null;
+        Node<T> curr = head;
+
+        while (curr != null) {
+            Node<T> temp = curr.next();
+            sorted = sorted_insert(sorted, curr);
+            curr = temp;
+        }
+        return sorted;
+    }
+
+    public static void main(String[] args) {
+        LinkedList<Integer> list = new LinkedList<>();
+
+        list.addFront(11);
+        list.addFront(82);
+        list.addFront(23);
+        list.addFront(29);
+
+        System.out.println(list);
+    }
+}
