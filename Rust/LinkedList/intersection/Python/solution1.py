4ab7adebcabe65ae5961df99a1efb8682b94d027
Zafar Takhirov <cc.rafaz@zafar.cc>
2015-09-20 14:29:17 -0400

    Finished python intersects

--- Rust/LinkedList/intersection/Python/solution1.py
+++ Rust/LinkedList/intersection/Python/solution1.py
@@ -3,45 +3,66 @@ sys.path.insert(0, '../..') # Need that to import LL
 
 from LinkedList import *    # Linked LIst defined here
 
-def sorted_insert(head, node):
-    if node == None:
-        return head
+def intersects(head1, head2):
+    l1 = None
+    l2 = None
 
-    if head == None or node.value() <= head.value():
-        node.setNext(head)
-        return node
+    l1len = get_length(head1)
+    l2len = get_length(head2)
 
-    curr = head
-    while curr.next() != None and curr.next().value() < node.value():
-        curr = curr.next()
+    d = l1len - l2len
 
-    node.setNext(curr.next())
-    curr.setNext(node)
+    l1 = head1 if d >= 0 else head2
+    l2 = head2 if d >= 0 else head1
+    d = d if d >= 0 else -d
 
-    return head
+    while d > 0:
+        l1 = l1.next()
+        d -= 1
 
-def insertion_sort(head):
-    sorted = None
-    curr = head
+    while l1 != None:
+        if l1 == l2:
+            return l1
 
-    while curr != None:
-        temp = curr.next()
-        sorted = sorted_insert(sorted, curr)
-        curr = temp
+        l1 = l1.next()
+        l2 = l2.next()
 
-    return sorted
+    return None
+
+def get_length(head):
+    list_length = 0
+    while head != None:
+        head = head.next()
+        list_length += 1
+    return list_length
 
 if __name__ == '__main__':
-    list = LinkedList()
-    sort = LinkedList()
+    list1 = LinkedList()
+    list2 = LinkedList()
+    list3 = LinkedList()
 
-    list.addFront(11)
-    list.addFront(82)
-    list.addFront(23)
-    list.addFront(29)
+    list1.addFront(27)
+    list1.addFront(12)
+    list1.addFront(23)
+    list1.addFront(29)
 
-    print list
-    sort.setHead(insertion_sort(list.head()))
-    print sort
-    print list
+    list2.addFront(4)
+    list2.addFront(13)
 
+    list2.head().next().setNext(list1.head().next().next())
+
+    list3.addFront(27)
+    list3.addFront(12)
+    list3.addFront(23)
+    list3.addFront(29)
+
+    print list1
+    print list2
+    print list3
+
+    # intsct = Node()
+    intsct = intersects(list1.head(), list2.head())
+    print intsct
+
+    intsct = intersects(list1.head(), list3.head())
+    print intsct
